1628f6d1bf3ba51228352ed3795822936eb8fe3a
Charles DZADU <dzaducharles@gmail.com>
2023-12-04 19:48:25 +0000

    feat: Add my informations

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Charles DZADU",
+  title: "Software Engineer",
+  email: "charles@dzadu.dev",
+  gitHub: "charlesdzadu",
+  instagram: "charlesdzadu",
+  linkedIn: "charlesdzadu",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "charlesdzadu",
+  youTube: "CharlesDzadu",
 };
 
 const primaryColor = "#4E567E";
